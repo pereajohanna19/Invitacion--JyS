d7cb5ab20770ec18560b407b3e9889b667519c91
pereajohanna19 <pereajohanna19@gmail.com>
2025-11-12 04:13:06 -0300

    Add files via upload
    archivos de la invitación boda

---
+++ config_xml.js
@@ -0,0 +1,46 @@
+var TSC = TSC || {};
+
+TSC.embedded_config_xml = '<x:xmpmeta tsc:version="2.0.1" xmlns:x="adobe:ns:meta/" xmlns:tsc="http://www.techsmith.com/xmp/tsc/">\
+   <rdf:RDF xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#" xmlns:xmp="http://ns.adobe.com/xap/1.0/" xmlns:xmpDM="http://ns.adobe.com/xmp/1.0/DynamicMedia/" xmlns:xmpG="http://ns.adobe.com/xap/1.0/g/" xmlns:xmpMM="http://ns.adobe.com/xap/1.0/mm/" xmlns:tscDM="http://www.techsmith.com/xmp/tscDM/" xmlns:tscIQ="http://www.techsmith.com/xmp/tscIQ/" xmlns:tscHS="http://www.techsmith.com/xmp/tscHS/" xmlns:stDim="http://ns.adobe.com/xap/1.0/sType/Dimensions#" xmlns:stFnt="http://ns.adobe.com/xap/1.0/sType/Font#" xmlns:exif="http://ns.adobe.com/exif/1.0" xmlns:dc="http://purl.org/dc/elements/1.1/">\
+      <rdf:Description dc:date="2025-11-05 02:53:08 AM" dc:source="Camtasia,25.2.4,esn" dc:title="Tarjeta de invitacion" tscDM:firstFrame="Tarjeta_de_invitacion_First_Frame.png" tscDM:project="Proyecto sin título">\
+         <xmpDM:duration xmpDM:scale="1/1000" xmpDM:value="63100"/>\
+         <xmpDM:videoFrameSize stDim:unit="pixel" stDim:h="1080" stDim:w="608"/>\
+         <tsc:langName>\
+            <rdf:Bag>\
+               <rdf:li xml:lang="es-ES">Spanish</rdf:li></rdf:Bag>\
+         </tsc:langName>\
+         <xmpDM:Tracks>\
+            <rdf:Bag>\
+               <rdf:li>\
+                  <rdf:Description xmpDM:trackType="Hotspot" xmpDM:frameRate="f1000" xmpDM:trackName="Hotspots">\
+                     <xmpDM:markers>\
+                        <rdf:Seq>\
+                           <rdf:li><rdf:Description xmpDM:label="1" xmpDM:startTime="48000" xmpDM:duration="5000" tscDM:boundingPoly="203,347;430,347;430,409;203,409;" tscDM:rotate="0.000000" tscHS:pause="0" xmpDM:location="https://maps.app.goo.gl/fdh7S7MMLpCQmYGf8" tscHS:newWindow="1"/></rdf:li><rdf:li><rdf:Description xmpDM:label="2" xmpDM:startTime="55000" xmpDM:duration="5000" tscDM:boundingPoly="228,294;405,294;405,348;228,348;" tscDM:rotate="0.000000" tscHS:pause="0" xmpDM:location="https://milistadenovios.cl/?gad_source=1&amp;gad_campaignid=22399530420&amp;gclid=CjwKCAjw04HIBhB8EiwA8jGNbb1vCnuIQDzpDXAG9nHQzP_hJbsF1o99NMX7e27EKQkU2zHe37dRbxoCZR4QAvD_BwE" tscHS:newWindow="1"/></rdf:li><rdf:li><rdf:Description xmpDM:label="3" xmpDM:startTime="39000" xmpDM:duration="3000" tscDM:boundingPoly="253,813;352,813;352,856;253,856;" tscDM:rotate="0.000000" tscHS:pause="0" xmpDM:location="https://maps.app.goo.gl/EGuvU334aWZY9BK48" tscHS:newWindow="1"/></rdf:li><rdf:li><rdf:Description xmpDM:label="4" xmpDM:startTime="48000" xmpDM:duration="5000" tscDM:boundingPoly="217,875;430,875;430,933;217,933;" tscDM:rotate="0.000000" tscHS:pause="0" xmpDM:location="https://docs.google.com/forms/d/e/1FAIpQLSdfvKnoOv0S2xOklvp1RaGBpApi7RKpagyLN_d63nxEBgJ4Kg/viewform?usp=preview" tscHS:newWindow="1"/></rdf:li></rdf:Seq>\
+                     </xmpDM:markers>\
+                  </rdf:Description>\
+               </rdf:li>\
+            </rdf:Bag>\
+         </xmpDM:Tracks>\
+         <tscDM:controller>\
+            <rdf:Description xmpDM:name="tscplayer">\
+               <tscDM:parameters>\
+                  <rdf:Bag>\
+                     <rdf:li xmpDM:name="autohide" xmpDM:value="true"/><rdf:li xmpDM:name="autoplay" xmpDM:value="false"/><rdf:li xmpDM:name="loop" xmpDM:value="false"/><rdf:li xmpDM:name="searchable" xmpDM:value="false"/><rdf:li xmpDM:name="captionsenabled" xmpDM:value="false"/><rdf:li xmpDM:name="sidebarenabled" xmpDM:value="false"/><rdf:li xmpDM:name="unicodeenabled" xmpDM:value="false"/><rdf:li xmpDM:name="backgroundcolor" xmpDM:value="000000"/><rdf:li xmpDM:name="sidebarlocation" xmpDM:value="left"/><rdf:li xmpDM:name="endaction" xmpDM:value="stop"/><rdf:li xmpDM:name="endactionparam" xmpDM:value="true"/><rdf:li xmpDM:name="locale" xmpDM:value="es-ES"/></rdf:Bag>\
+               </tscDM:parameters>\
+               <tscDM:controllerText>\
+                  <rdf:Bag>\
+                  </rdf:Bag>\
+               </tscDM:controllerText>\
+            </rdf:Description>\
+         </tscDM:controller>\
+         <tscDM:contentList>\
+            <rdf:Description>\
+               <tscDM:files>\
+                  <rdf:Seq>\
+                     <rdf:li xmpDM:name="0" xmpDM:value="Tarjeta_de_invitacion_First_Frame.png"/></rdf:Seq>\
+               </tscDM:files>\
+            </rdf:Description>\
+         </tscDM:contentList>\
+      </rdf:Description>\
+   </rdf:RDF>\
+</x:xmpmeta>';
